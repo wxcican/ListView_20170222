c0137798a5d3d5e99493397ca53aa6a9e35536f2
wxcican <wxcican@qq.com>
2017-02-24 11:06:46 +0800

    当用户滑动到listView底部时，自动加载数据

--- app/src/main/java/com/fuicuiedu/xc/listview_20170222/MainActivity.java
+++ app/src/main/java/com/fuicuiedu/xc/listview_20170222/MainActivity.java
@@ -3,9 +3,11 @@ package com.fuicuiedu.xc.listview_20170222;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListAdapter;
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private List<String> datas;
     private ArrayAdapter<String> adapter;
     private Handler handler;
+    private int visblelastindex;// 可见的最后一条数据下标
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +45,95 @@ public class MainActivity extends AppCompatActivity {
 
         mLv.setAdapter(adapter);
 
-        addMoreView(mLv);
+        //给listview加一个底部布局，点击即加载数据
+//        addMoreView(mLv);
 
         //动态的设置ListView高度
 //        setListViewHeight(mLv);
+
+        mLv.setOnScrollListener(listener);
     }
+
+
+    //ListView三种滑动状态
+    //手指按下移动的状态
+    //SCROLL_STATE_TOUCH_SCROLL //触摸滑动
+    //惯性滚动状态
+    //SCROLL_STATE_FLING //滑翔
+    //静止状态
+    //SCROLL_STATE_IDLE //静止
+
+
+    //目的：当用户滑动到listView底部时，自动加载数据
+    //实现：判断是否是静止状态，判断是否滑动到了底部（最后一条数据）
+    private AbsListView.OnScrollListener listener = new AbsListView.OnScrollListener() {
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+        //当滚动状态改变时触发的方法
+        //最后一条数据的下标
+        int lastIndex = adapter.getCount() - 1;
+        //判断是否是静止状态，判断是否滑动到了底部（最后一条数据）
+        if (scrollState == SCROLL_STATE_IDLE && visblelastindex == lastIndex){
+            //加载数据
+            loadMoreData();
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+        //参数说明
+        //firstVisibleItem//第一个可见的item
+        //visibleItemCount//可见的item数量
+        //totalItemCount//所有item数量
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        //当listview滑动时触发的方法
+
+        visblelastindex = firstVisibleItem + visibleItemCount - 1;
+
+        Log.e("======================","=======================");
+        Log.e("firstVisibleItem = ",firstVisibleItem + "");
+        Log.e("visibleItemCount = ",visibleItemCount + "");
+        Log.e("totalItemCount = ",totalItemCount + "");
+        Log.e("======================","=======================");
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //给listview加一个底部布局，点击即加载数据
     private void addMoreView(ListView listView){
